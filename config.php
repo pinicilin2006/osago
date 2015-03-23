d19af4adfc73a3077884a1da7a02b8e0e4e7f9c9
husaniov_aa <husaniov_aa@sngi.ru>
2015-03-23 16:38:50 +0500

    Начало работы по импорту БСО из IBS

--- config.php
+++ config.php
@@ -1,8 +1,17 @@
 <?php
+//Данные для подключения к mysql
 $dbhost = "localhost";
 $dbbase = "osago";
 $dbuser = "osago";
 $dbpass = "aq1sw2de3";
+///////////////////////////////
+//Данные для подключения к oracle
+$oracle_host    = "10.250.245.73"; // Имя машины, где размещена СУБД Oracle
+$oracle_sid     = "sngi"; // Имя сервиса(SID, SERVICE_NAME) экземпляра СУБД Oracle
+$oracle_port    = 1521 ; // Порт для работы с СУБД ORACLE
+$oracle_db_user = "ins";
+$oracle_db_psw  = "adtf332";
+/////////////////////////////////
 $send_message = array(
 	'husainov_aa@sngi.ru',
 	'aksenov_pv@sngi.ru', 
